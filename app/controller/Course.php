cd307c71c618e7762e1870163e8c04ffbac94d0c
ikozn <ikozn@protonmail.com>
2020-12-01 22:22:09 +0800

    fix: 修复创建课程API创建失败的BUG

--- app/controller/Course.php
+++ app/controller/Course.php
@@ -49,4 +49,35 @@ class Course extends Base
         //6. 返回课程信息
         return $this->build($course, "成功");
     }
+
+    public function createClass()
+    {
+        $receive_field = ['title', 'describ', 'course_id'];  //接收字段
+        $visible_field = ['id', 'course_id', 'title', 'describ'];  //输出字段
+        $write_field = $visible_field; //写入字段
+
+
+        //1. 获取USER ID
+        $userId = request()->uid;
+
+        //2. 获取提交信息
+        $courseData = Request::only($receive_field, 'post');
+
+        //3. 数据整形
+        $courseData["describ"] = trim($courseData["describ"]);
+        $courseData["title"] = trim($courseData["title"]);
+
+        //4. 校验数据
+        try {
+            validate(CourseVerify::class)->batch(true)->scene('create')->check($courseData);
+        } catch (ValidateException $e) {
+            return $this->build($e->getError(), "参数错误")->code(400);
+        }
+
+        //5. 添加记录
+        $course = CourseModel::create($courseData, $write_field)->visible($visible_field);
+
+        //6. 返回课程信息
+        return $this->build($course, "成功");
+    }
 }
