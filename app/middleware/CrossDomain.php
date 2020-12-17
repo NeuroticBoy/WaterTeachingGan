c3ff4e38d7936f629e268ccceed4e20d2d133064
ikozn <ikozn@protonmail.com>
2020-12-17 22:33:17 +0800

    refactor: 重构跨域请求处理中间件

---
+++ app/middleware/CrossDomain.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\middleware;
+
+
+/**
+ * 全局跨域请求处理
+ * Class CrossDomain
+ * @package app\middleware
+ */
+
+class CrossDomain
+{
+    public function handle($request, \Closure $next)
+    {
+        $response = $next($request);
+
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Max-Age: 1800');
+        header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Access-Token, Authorization, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-CSRF-TOKEN, X-Requested-With');
+        // OPTIONS请求返回204请求
+        if (strtoupper($request->method()) === "OPTIONS") {
+            return $response->code(204);
+        }
+
+        return $next($request);
+    }
+}
