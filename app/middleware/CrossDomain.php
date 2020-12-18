c296b0a9a958bb81d6d695dec8bc9ffb7820553b
ikozn <ikozn@protonmail.com>
2020-12-18 09:03:36 +0800

    fix: 修复跨域请求处理重复处理请求的BUG

--- app/middleware/CrossDomain.php
+++ app/middleware/CrossDomain.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace app\middleware;
 
+use think\Response;
 
 /**
  * 全局跨域请求处理
@@ -15,7 +16,6 @@ class CrossDomain
 {
     public function handle($request, \Closure $next)
     {
-        $response = $next($request);
 
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Max-Age: 1800');
@@ -23,7 +23,7 @@ class CrossDomain
         header('Access-Control-Allow-Headers: Access-Token, Authorization, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-CSRF-TOKEN, X-Requested-With');
         // OPTIONS请求返回204请求
         if (strtoupper($request->method()) === "OPTIONS") {
-            return $response->code(204);
+            return Response::create()->send();
         }
 
         return $next($request);
